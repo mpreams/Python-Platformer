a622c3372a6fe0a273baa3e6dfbf85f72089fdf5
Matthew Reams <matthewreams@Matthews-MacBook-Pro.local>
2025-06-07 02:21:30 -0400

    Init game loop

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+import os
+import random
+import math
+import pygame
+from os import listdir
+from os.path import isfile, join
 
+pygame.init()
+pygame.display.set_caption("Platformer")
+
+BG_COLOR = (135, 206, 235)  # Sky blue
+WIDTH, HEIGHT = 1000, 800
+FPS = 60
+PLAYER_SPEED = 5
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main(window):
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                running = False
+                break
+
+
+if __name__ == "__main__":
+    main(window)
